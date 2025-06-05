37b5445ddcbaa709c4a91e0a4a83e7fbc899f14e
金子 明彦 <akihiko.kaneko@csri-japan.com>
2025-06-05 16:48:20 +0900

    login

--- models.py
+++ models.py
@@ -1,20 +1,17 @@
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
-    role = db.Column(db.String(50), default='user')
-
-def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    # 参加者とのリレーション
+    participants = db.relationship('Participant', backref='user', lazy=True)
 
 class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    position = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.String(10), nullable=False)  # '出席' or '欠席'
+    position = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    status = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所有者ユーザー
