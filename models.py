a8a1bb22192c770e49c023c738b7501b251b411a
金子 明彦 <akihiko.kaneko@csri-japan.com>
2025-06-05 17:25:01 +0900

    login

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin  # ← 追加
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)  # ← これを追加
+    password_hash = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(50), nullable=False)
-    # 参加者とのリレーション
     participants = db.relationship('Participant', backref='user', lazy=True)
 
 class Participant(db.Model):
